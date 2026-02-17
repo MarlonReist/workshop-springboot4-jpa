af3074fb128cb8a4696d04df1e2e5b399626dee1
Marlon Reis <marlonreisdev@gmail.com>
2026-02-17 14:29:48 -0300

    Project created

---
+++ src/main/java/Demo/Spring/Boot/MarlonApiApplication.java
@@ -0,0 +1,13 @@
+package Demo.Spring.Boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarlonApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarlonApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Demo/Spring/Boot/MarlonApiApplicationTests.java
@@ -0,0 +1,13 @@
+package Demo.Spring.Boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarlonApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
