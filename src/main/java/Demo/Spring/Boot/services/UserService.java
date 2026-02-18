d5f5df4cd95d578f53fde35c2d41b8566d51a07c
Marlon Reis <marlonreisdev@gmail.com>
2026-02-18 14:46:06 -0300

    Exception handling Update

--- src/main/java/Demo/Spring/Boot/services/UserService.java
+++ src/main/java/Demo/Spring/Boot/services/UserService.java
@@ -4,6 +4,7 @@ import Demo.Spring.Boot.entities.User;
 import Demo.Spring.Boot.repositories.UserRepository;
 import Demo.Spring.Boot.services.exceptions.DatabaseException;
 import Demo.Spring.Boot.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -42,12 +43,14 @@ public class UserService {
     }
 
     public User update(Long id, User obj) {
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
-
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
-
     private void updateData(User entity, User obj) {
         entity.setName(obj.getName());
         entity.setEmail(obj.getEmail());
